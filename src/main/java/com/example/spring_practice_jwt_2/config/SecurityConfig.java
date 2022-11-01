f8108a9a17245c67b93b2012c9421aeb67f18361
2sssg <lsg020300@naver.com>
2022-11-01 15:46:40 +0900

    feat: spring security

---
+++ src/main/java/com/example/spring_practice_jwt_2/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.spring_practice_jwt_2.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web -> web
+				.ignoring()
+				.antMatchers("/favicon")
+				.antMatchers("h2-console")
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+				.csrf().disable();
+		return httpSecurity.build();
+	}
+}
