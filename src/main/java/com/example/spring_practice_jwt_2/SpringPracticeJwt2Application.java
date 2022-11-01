0d96d7bef30917a9731b1adc5961bf9b48843cf6
2sssg <lsg020300@naver.com>
2022-11-01 10:20:14 +0900

    initialize

---
+++ src/main/java/com/example/spring_practice_jwt_2/SpringPracticeJwt2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_jwt_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeJwt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeJwt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_practice_jwt_2/SpringPracticeJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_jwt_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeJwt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
